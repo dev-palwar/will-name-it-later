785c9681075a9c83f463da1fb4506ba120b985f4
dev-palwar <devpalwar06@gmail.com>
2025-01-30 00:06:29 +0530

    added responsiveness alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,65 @@
+"use client";
 import Hero from "@/components/hero/hero";
 import Section2 from "@/components/section-2";
 import Section3 from "@/components/section-3";
 import Section4 from "@/components/section-4";
 import Footer from "@/components/footer";
+import ResponsivenessAlert from "@/components/responsiveness-alert";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [displayAlert, setDisplayAlert] = useState<boolean>(false);
+  const [windowSize, setWindowSize] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  console.log(windowSize);
+
+  const handleResize = () => {
+    setWindowSize({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+  };
+
+  const handleDisplayAlert = () => {
+    if (windowSize.width < 1531 || windowSize.height < 800) {
+      setDisplayAlert(true);
+    } else {
+      setDisplayAlert(false);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      handleResize();
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    handleDisplayAlert();
+  }, [windowSize]);
+
   return (
     <>
-      <Hero />
-      <Section3 />
-      <Section4 />
-      <Section2 />
-      <Footer />
+      {displayAlert ? (
+        <ResponsivenessAlert />
+      ) : (
+        <>
+          <Hero />
+          <Section3 />
+          <Section4 />
+          <Section2 />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/responsiveness-alert.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResponsivenessAlert: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      Responsiveness under construction 🚧{" "}
+    </div>
+  );
+};
+
+export default ResponsivenessAlert;
